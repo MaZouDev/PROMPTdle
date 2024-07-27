b64fc48c47f4c0081ab7259900d8e36af2cb6268
MaZoo <mazoodev@gmail.com>
2024-07-27 18:03:05 +0200

    add input field

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import {ThemeProvider} from "next-themes";
 const inter = Inter({subsets: ["latin"]});
 
 function Header() {
-    return <header className="w-full flex justify-between py-4">
+    return <header className="w-full flex justify-between p-4">
         <h1><strong>PROMPT</strong>dle</h1>
         <ThemeToggle/>
     </header>;
@@ -15,19 +15,19 @@ function Header() {
 
 function Footer() {
     return (
-        <footer className="mt-auto flex justify-between py-4 pt-24 overflow-y-clip">
-            <h1>Made by. Hosted by.</h1>
-
+        <footer className="mt-auto flex justify-between p-4">
+            <h1>Made by MaZoo. Hosted by.</h1>
             <div>
                 <Image
                     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
                     src="/next.svg"
                     alt="Next.js Logo"
-                    width={180}
-                    height={37}
+                    width={0}
+                    height={0}
+                    style={{ width: 'auto', height: '2vh' }}
                     priority
                 />
-                <div className="relative z-[-1]
+                <div className="fixed z-[-1]
             after:absolute after:-z-20 after:h-[120px] after:w-full after:-translate-y-1/2 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-['']
             after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
                 </div>
