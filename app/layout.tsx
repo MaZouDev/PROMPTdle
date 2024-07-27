6545c8826862923eefc14e4691346551622479fb
MaZoo <mazoodev@gmail.com>
2024-07-27 15:15:45 +0200

    add react dev tools script

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,11 +43,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+        <head>
+            <script src="http://localhost:8097"></script>
+        </head>
         <body className={inter.className + " max-w-screen-lg m-auto min-h-screen flex flex-col"}>
         <ThemeProvider>
-        <Header/>
+            <Header/>
             {children}
-        <Footer/>
+            <Footer/>
         </ThemeProvider>
         </body>
         </html>
